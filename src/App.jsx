6bcebc966ccf541a6d78df0da2fba7430fb671d4
Hamid-200 <hamidadio2005@gmail.com>
2026-05-20 18:22:19 +0100

    overwrite after eval added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
@@ -65,6 +72,7 @@ function reducer(state, { type, payload }) {
       }
       return {
         ...state,
+        overwrite: true,
         previousOperand: null,
         operator: null,
         currentOperand: evaluate(state),
